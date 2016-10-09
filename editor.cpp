8ce06103b5c5848b48dd31e21b6efd3eefed03d7
Jan Tuomi <jan-sebastian.tuomi@aalto.fi>
2016-10-09 13:28:54 +0300

    Add line editing

--- editor.cpp
+++ editor.cpp
@@ -1,5 +1,7 @@
 #include "editor.h"
 #include "utils.h"
+#include <limits>
+#include <iostream>
 
 Editor::Editor(std::vector<std::string>* contents):
     m_current_line(0)  {
@@ -15,12 +17,45 @@ int Editor::run_editor() {
         print_contents_on_line(m_current_line);
         
         char input = Utils::getch();
+        switch (input) {
+        case 'e':
+            edit_command();
+            break;
+        default:
+            break;
+        }
     }
     return 0;
 }
 
 int Editor::print_contents_on_line(int line) const {
-    for (int i = line; i < m_contents->size(); i++) {
+    for (int i = line; i < std::min(m_contents->size(), line + OUTPUT_LINE_MAX); i++) {
         std::cout << i << " " << m_contents->at(i) << '\n';
     }
+}
+
+int Editor::edit_command() {
+    std::cout << "Edit which line? ";
+    int line;
+    std::cin >> line;
+    if (std::cin.fail() || line < 0 || line >= m_contents->size()) {
+        std::cout << "Not a valid line number!\n";
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+        return 1;
+    }
+
+    edit_line(line);
+}
+
+int Editor::edit_line(int line) {
+    std::cout << "> ";
+    std::string input;
+
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+    std::getline(std::cin, input);
+    std::cin.clear();
+    (*m_contents)[line] = input;
+
+    return 0;
 }
--- editor.h
+++ editor.h
@@ -9,10 +9,14 @@ class Editor {
         Editor(std::vector<std::string>* contents);
         int run_editor();
     private:
+        const static int OUTPUT_LINE_MAX = 10;
+
         std::vector<std::string>* m_contents;
         int m_current_line;
 
         int print_contents_on_line(int line) const;
+        int edit_command();
+        int edit_line(int line);
 };
 
 #endif
